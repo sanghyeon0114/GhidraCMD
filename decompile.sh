45ede82ffe28901baf27a7fe4b77c7c30633f13e
황상현 <sanghyeon@hwangsanghyeon-ui-MacBookPro.local>
2025-02-18 21:18:57 +0900

    프로젝트에 데이터가 쌓이지 않도록 함

--- decompile.sh
+++ decompile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./ghidra/*/support/analyzeHeadless ./projects decompile -import $1 -overwrite -postScript decompile.py
-#./ghidra/*/support/analyzeHeadless ./projects decompile -process chall0.exe -postScript decompile.py
+./ghidra/*/support/analyzeHeadless ./projects decompile -import $1 -deleteProject -overwrite -postScript decompile.py
